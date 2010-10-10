c7b7973bd039931fd65335caa6b1deb8ae7a70dd
Eric Allam <rubymaverick@gmail.com>
2010-10-10 17:20:17 -0400

    Added admin views into the repo

--- app/controllers/admin/events_controller.rb
+++ app/controllers/admin/events_controller.rb
@@ -4,4 +4,10 @@ class Admin::EventsController < Admin::ResourcesController
     @event = Event.find params[:id]
     render :action => 'events/show', :layout => 'application'
   end
+
+  def view_public
+    @event = Event.find params[:id]
+
+    redirect_to event_path(@event)
+  end
 end
