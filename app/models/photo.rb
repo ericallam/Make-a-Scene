0eb734539c8744777d98b9bac1d8a6a0da50ac93
Eric Allam <rubymaverick@gmail.com>
2010-10-26 23:03:35 -0400

    first try

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,10 +1,17 @@
+require 'paperclip_processors/watermark'
+
 class Photo < ActiveRecord::Base
   extend S3AttachedFile
 
   has_many :photo_shares
   belongs_to :event
   
-  has_attached_s3_file :image, :styles => { :small => "100>", :big => "x400>", :album => "80x80#", :external => "90%" }, :convert_options => { :all => '-strip' }
+  has_attached_s3_file :image, :styles => { 
+    :small => { :geometry => "100>", :processors => [:thumbnail] }, 
+    :big => { :geometry => "x400>", :watermark_path => "#{Rails.root}/public/images/watermark_big.png", :position => 'south', :processors => [:watermark] }, 
+    :album => { :geometry => "80x80#", :processors => [:thumbnail] }, 
+    :external => { :geometry => "90%" , :watermark_path => "#{Rails.root}/public/images/watermark_external.png", :position => 'south', :processors => [:watermark] }
+  }, :convert_options => {:all => "-strip"}
 
   # small = width: 100px, max-height: 150
   # big = width: 325px; max-height: 490 
