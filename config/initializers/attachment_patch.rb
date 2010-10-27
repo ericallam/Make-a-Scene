bbbe22d0626af1bd8c6ae3dd235503b3936c9997
Eric Allam <rubymaverick@gmail.com>
2010-10-27 12:12:07 -0400

    skip style if no geometry present

--- config/initializers/attachment_patch.rb
+++ config/initializers/attachment_patch.rb
@@ -6,8 +6,12 @@ module Paperclip
           raise RuntimeError.new("Style #{name} has no processors defined.") if style.processors.blank?
           @queued_for_write[name] = style.processors.inject(@queued_for_write[:original]) do |file, processor|
             puts 'style: ' + style.processor_options.inspect
-            Paperclip.processor(processor).make(file, style.processor_options, self)
-          end
+            if style.processor_options[:geometry].present?
+              Paperclip.processor(processor).make(file, style.processor_options, self)
+            else
+              nil
+            end
+          end.compact
         rescue PaperclipError => e
           log("An error was received while processing: #{e.inspect}")
           (@errors[:processing] ||= []) << e.message if @whiny
