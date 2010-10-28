febee37c84a4683698ea5a4b1add5398a6df26ba
Eric Allam <rubymaverick@gmail.com>
2010-10-27 22:13:38 -0400

    jcarousel the front page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
   
   def index
-    
+    @images = Dir["#{Rails.root}/public/images/carousel/*.jpg"].map {|p| p.gsub("#{Rails.root}/public/images/", "") }
   end
   
 end
